4f4c7fc12cd15a3f2389077a2e6c3aeba43a5baa
Jon Taylor <31380713+DevJonTaylor@users.noreply.github.com>
2022-01-26 21:39:39 -0600

    init push

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// TODO:  Display Date at the top of the page.
+// TODO:  Create time blocks for standard business hours. (9-5)
+// TODO:  Time blocks are editablecontent.
+// TODO:  Confirm save modal.
+// TODO:  localStorage to save the data on refresh.
+
